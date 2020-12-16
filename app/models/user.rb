54f816db707e72cb0e0d20adf70a1a3536226b34
Tayyab Athar <tayyabathar@Tayyabs-MacBook-Pro.local>
2020-12-17 04:26:48 +0500

    [YTS-2] validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,7 @@ class User < ApplicationRecord
 
   has_many :ratings
   has_many :rated_movies, through: :feedback, class_name: :Movie
+
+  validates :user_name, presence: true, uniqueness: true
+  validates :name, presence: true
 end
