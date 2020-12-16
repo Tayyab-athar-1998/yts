9be845876489fa84b405402aeae51b6874270764
Tayyab Athar <tayyabathar@Tayyabs-MacBook-Pro.local>
2020-12-17 03:53:24 +0500

    [YTS-1] reordering assoication across user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :movies_created, class_name: :Movie, inverse_of: 'created_by'
 
+  has_one :image, as: :imageable
   has_many :feedback
   has_many :movies, through: :feedback
+  has_many :movies_created, class_name: :Movie, inverse_of: 'created_by'
 
   has_many :ratings
   has_many :rated_movies, through: :feedback, class_name: :Movie
-
-  has_one :image, as: :imageable
 end
