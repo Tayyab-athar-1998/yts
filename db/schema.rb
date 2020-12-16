18af3868a767570463a48bfc21c9bb57d4bc8a88
Tayyab Athar <tayyabathar@Tayyabs-MacBook-Pro.local>
2020-12-17 04:14:34 +0500

    [YTS-1] adding language column to movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201216104117) do
+ActiveRecord::Schema.define(version: 20201216231052) do
 
   create_table "actors", force: :cascade do |t|
     t.string "name"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 20201216104117) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.text "languages"
     t.index ["user_id"], name: "index_movies_on_user_id"
   end
 
