0e25bf9c279a61df9a3cde0f4f133e2b6b795436
Tayyab Athar <tayyabathar@Tayyabs-MacBook-Pro.local>
2020-12-17 03:53:07 +0500

    [YTS-1] reordering assoication across models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,22 +1,19 @@
 class Movie < ApplicationRecord
+  has_one :cover_photo, class_name: :Image, as: :imageable
+
+  has_many :ratings
+  has_many :feedback
+  has_many :movie_roles
+  has_many :roles, through: :movie_roles
+  has_many :user, through: :feedback
+  has_many :rated_by_user, through: :ratings, class_name: :User
+  has_many :attached_images, class_name: :Image, as: :imageable
+  has_many :attached_images, class_name: :Image, as: :imageable
+  has_many :likes, class_name: :Like, as: :likeable
+
   has_and_belongs_to_many :genre
   has_and_belongs_to_many :language
   has_and_belongs_to_many :video_quality
 
   belongs_to :created_by, class_name: :User, inverse_of: 'movies_created', foreign_key: 'user_id'
-
-  has_many :movie_roles
-  has_many :roles, through: :movie_roles
-
-  has_many :feedback
-  has_many :user, through: :feedback
-
-  has_many :ratings
-  has_many :rated_by_user, through: :ratings, class_name: :User
-
-  has_many :attached_images, class_name: :Image, as: :imageable
-  has_one :cover_photo, class_name: :Image, as: :imageable
-
-  has_many :attached_images, class_name: :Image, as: :imageable
-  has_many :likes, class_name: :Like, as: :likeable
 end
