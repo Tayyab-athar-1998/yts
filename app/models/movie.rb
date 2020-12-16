e1f558e66507c1142f90cd9a0ed4906d855cd577
Tayyab Athar <tayyabathar@Tayyabs-MacBook-Pro.local>
2020-12-17 04:50:17 +0500

    [YTS-2] adding validation for movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -15,4 +15,19 @@ class Movie < ApplicationRecord
   has_and_belongs_to_many :video_quality
 
   belongs_to :created_by, class_name: :User, inverse_of: 'movies_created', foreign_key: 'user_id'
+
+  validates :name, presence: true
+  validates :release_date, presence: true
+  validates :release_date_is_date
+  validates :synopsis, presence: true
+  validates :languages, presence: true, inclusion: {
+    in: %w[english English Hindi hindi],
+    message: '%<value> is not a allowed language'
+  }
+
+  private
+
+  def release_date_is_date?
+    errors.add(:release_date, 'must be a valid date') unless release_date.is_a?(Date)
+  end
 end
