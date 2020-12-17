c3ab31df36b13bcb3a30f8ecdee683e0fa818e7e
Tayyab Athar <tayyabathar@Tayyabs-MacBook-Pro.local>
2020-12-17 06:07:51 +0500

    [YTS-2] adding validations for movie roles

--- app/models/movie_role.rb
+++ app/models/movie_role.rb
@@ -1,4 +1,10 @@
 class MovieRole < ApplicationRecord
   belongs_to :movie
   belongs_to :actor
+
+  validates :role_played, presence: true, inclusion: {
+    in: %w[actor director],
+    message: '%<value> is not a allowed role'
+  }
+  validates :staring_as, presence: true, if: :role_played == 'actor'
 end
