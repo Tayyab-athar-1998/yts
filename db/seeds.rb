e818724eb237396337d71292cb8efc83737f845b
Tayyab Athar <tayyabathar@Tayyabs-MacBook-Pro.local>
2020-12-18 00:48:48 +0500

    adding genre seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,6 @@ puts "Please don't close, it may take few minutes......;"
 
 puts 'Seeding user accounts....'
 load "#{Rails.root}/db/seeds/users.rb"
+
+puts 'Seeding genre accounts....'
+load "#{Rails.root}/db/seeds/genres.rb"
---
+++ db/seeds/genres.rb
@@ -0,0 +1,3 @@
+Genre.where(name: 'Action').first_or_create!(name: 'Action')
+Genre.where(name: 'SIFI').first_or_create!(name: 'SIFI')
+Genre.where(name: 'Comedy').first_or_create!(name: 'Comedy')
