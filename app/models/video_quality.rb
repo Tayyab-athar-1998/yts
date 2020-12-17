dacac5fe9469b7f21601cb5d32d4093a551a47c8
Tayyab Athar <tayyabathar@Tayyabs-MacBook-Pro.local>
2020-12-18 02:11:40 +0500

    [YTS-2] replacing inclusion in video quality with uniqueness

--- app/models/video_quality.rb
+++ app/models/video_quality.rb
@@ -1,8 +1,5 @@
 class VideoQuality < ApplicationRecord
   has_and_belongs_to_many :movie
 
-  validates :resolution, presence: true, inclusion: {
-    in: %w[720p 1080p 4K],
-    message: '%<value> is not a allowed resolution.'
-  }
+  validates :resolution, presence: true, uniqueness: true
 end
