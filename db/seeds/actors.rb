2d1ba635e3a6bda1a9cc24345ae4589e97825799
Tayyab Athar <tayyabathar@Tayyabs-MacBook-Pro.local>
2020-12-18 03:25:00 +0500

    [YTS-2] minor

--- db/seeds/actors.rb
+++ db/seeds/actors.rb
@@ -1,3 +1,3 @@
 Actor.where(name: 'Leo').first_or_create!(name: 'Leo')
 Actor.where(name: 'Tayyab').first_or_create!(name: 'Tayyab')
-Actors.where(name: 'Hamza').first_or_create!(name: 'Hamza')
+Actor.where(name: 'Hamza').first_or_create!(name: 'Hamza')
