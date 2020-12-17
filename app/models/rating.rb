48c2f6fe899ec0a69382ce94398b7af1bce06f23
Tayyab Athar <tayyabathar@Tayyabs-MacBook-Pro.local>
2020-12-17 05:46:19 +0500

    [YTS-2] adding validations for rating model

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,7 @@
 class Rating < ApplicationRecord
   belongs_to :movie
   belongs_to :user
+
+  validates :value, presence: true
+  validate ->(object) { errors.add(:value, "rating can't be negative.") if object.value < 0.0 }
 end
