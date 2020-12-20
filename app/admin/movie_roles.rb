40ad36ee9f952845cef489870d92eaf499c588e8
Tayyab Athar <tayyabathar@Tayyabs-MacBook-Pro.local>
2020-12-21 04:17:08 +0500

    active admin for MovieRoles

---
+++ app/admin/movie_roles.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register MovieRole do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :staring_as, :role_played, :movie_id, :actor_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:staring_as, :role_played, :movie_id, :actor_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
