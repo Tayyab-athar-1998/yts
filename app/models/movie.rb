9250bb77094ce52a08c1a002bdf0961fb1f21026
Tayyab Athar <tayyabathar@Tayyabs-MacBook-Pro.local>
2020-12-23 09:17:08 +0500

    removing has_many through when direct query is not needed

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,9 +4,6 @@ class Movie < ApplicationRecord
   has_many :ratings
   has_many :feedback
   has_many :movie_roles
-  has_many :roles, through: :movie_roles
-  has_many :user, through: :feedback
-  has_many :rated_by_user, through: :ratings, class_name: :User
   has_many :attached_images, class_name: :Image, as: :imageable
   has_many :likes, class_name: :Like, as: :likeable
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,10 @@ class User < ApplicationRecord
 
   has_one :image, as: :imageable
   has_many :feedback
-  has_many :movies, through: :feedback
   has_many :movies_created, class_name: :Movie, inverse_of: 'created_by'
 
   has_many :ratings
-  has_many :rated_movies, through: :feedback, class_name: :Movie
-
+  
   has_many :movies, through: :likes, source: :likeable, source_type: 'Movie'
   has_many :comments, through: :likes, source: :likeable, source_type: 'Comment'
 end
