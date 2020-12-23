35ebe2531de31147b6e9359eaf0636cd7ce6fc86
Tayyab Athar <tayyab.athar@sendoso.com>
2020-12-23 11:10:03 +0500

    adding

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :image, as: :imageable
-  has_many :feedback
-  has_many :movies_created, class_name: :Movie, inverse_of: 'created_by'
+  has_one :image, as: :imageable, dependent: :destroy
+  has_many :feedback, dependent: :destroy
+  has_many :movies_created, class_name: :Movie, inverse_of: 'created_by', dependent: :destroy
 
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
   
-  has_many :movies, through: :likes, source: :likeable, source_type: 'Movie'
-  has_many :comments, through: :likes, source: :likeable, source_type: 'Comment'
+  has_many :movies, through: :likes, source: :likeable, source_type: 'Movie', dependent: :destroy
+  has_many :comments, through: :likes, source: :likeable, source_type: 'Comment', dependent: :destroy
 end
--- app/models/video_quality.rb
+++
@@ -1,3 +0,0 @@
-class VideoQuality < ApplicationRecord
-  has_and_belongs_to_many :movie
-end
