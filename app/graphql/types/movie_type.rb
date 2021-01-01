baef0eae3dd1211af9a67c71b4b81a789c87830b
Tayyab Athar <tayyab.athar@sendoso.com>
2021-01-01 15:15:29 +0500

    adding movie mutation

--- app/graphql/types/movie_type.rb
+++ app/graphql/types/movie_type.rb
@@ -4,10 +4,11 @@ module Types
 
     field :id, ID, null: false
     field :name, String, null: false
-    field :number_of_downloads, String, null: false
+    field :number_of_downloads, Int, null: false
     field :release_date, String, null: false
     field :is_featured, Boolean, null: false
     field :likes_count, Int, null: true
+    field :synopsis, String, null: false
 
     field :created_by, Types::UserType, null: false
     field :rating, Float, null: true
