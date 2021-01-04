a9fa57db2b14367063c2fcb80bf9ced6cb5d54e1
Tayyab Athar <tayyab.athar@sendoso.com>
2021-01-04 13:36:18 +0500

    add configuration for devise token

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :token_authenticatable
 
   has_one :image, as: :imageable, dependent: :destroy
   has_many :feedback, dependent: :destroy
